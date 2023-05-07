7aba4959f19eaadfefac5ebc5813aeed808d3a4d
Ahemd Yasser <102255667+Ahmedyasser10@users.noreply.github.com>
2023-05-07 21:53:25 +0300

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,41 @@
+class Address {
+    private String city;
+    private String Street;
+    private int HouseNumber;
+    private int ApartmentNumber;
+
+
+    public int getApartmentNumber() {
+        return ApartmentNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getHouseNumber() {
+        return HouseNumber;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void SetApartment(int ApartmentNumber) {
+        this.ApartmentNumber = ApartmentNumber;
+    }
+
+    public void SetCity(String city) {
+        this.city = city;
+    }
+
+    public void SetHouseNumber(int HouseNumber) {
+        this.HouseNumber = HouseNumber;
+    }
+
+    public void SetStreet(String Street) {
+        this.Street = Street;
+    }
+
+
+}
