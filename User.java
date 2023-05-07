559646ce2b0da60d50f37a182989237647cc061f
Ahemd Yasser <102255667+Ahmedyasser10@users.noreply.github.com>
2023-05-07 21:53:41 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,62 @@
+public class User {
+    private String name;
+    private String UserID;
+    private String PhoneNumber;
+    private String Password;
+    private Address Add;
+
+    private boolean LoggedIn;
+
+    public User() {
+        LoggedIn = false;
+        Add = new Address();
+    }
+
+    public void Login(){
+        LoggedIn = true;
+    }
+    public void SetName(String name) {
+        this.name = name;
+    }
+
+    public void SetUserID(String UserID) {
+        this.UserID = UserID;
+    }
+
+    public void SetPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public void SetPassword(String UserPassword) {
+        this.Password = UserPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public Address getAdd() {
+        return Add;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void SetAddress(String city, String Street, int HouseNumber, int ApartmentNumber) {
+        Add.SetCity(city);
+        Add.SetApartment(ApartmentNumber);
+        Add.SetStreet(Street);
+        Add.SetHouseNumber(HouseNumber);
+    }
+
+
+}
